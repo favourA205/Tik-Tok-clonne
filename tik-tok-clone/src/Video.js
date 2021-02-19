4bde6f6eea06c87ef53c1857568d9b5ce7c61f4c
favourA205 <favouragbejule@gmail.com>
2021-02-19 16:41:04 +0100

    Tik tok clone frontend functionality added

---
+++ tik-tok-clone/src/Video.js
@@ -0,0 +1,37 @@
+import React, { useState, useRef } from 'react';
+import './Video.css';
+import VideoFooter from'./VideoFooter';
+
+function Video() {
+    const [playing, setPlaying] =useState(false)
+    const videoRef = useRef(null);
+
+    const onVideoPress = () => {
+        if(playing){
+        videoRef.current.pause();
+        setPlaying(false);
+        } else{
+          videoRef.current.play();
+          setPlaying(true);
+        }
+    };
+
+
+    return (
+        <div className="video">
+            <video 
+                className="video__player" 
+                loop
+                onClick={onVideoPress}
+                ref={videoRef}
+                src=""
+                ></video>
+
+            <VideoFooter/>
+            {/* <VideoFooter/> */}
+            {/* <VideoSidebar/> */}
+        </div>
+    )
+}
+
+export default Video;
